535c46637f91b5b8988b3fb083ea3d0c79aa9b0c
KoToValery <kostadintosev@gmail.com>
2024-12-11 10:33:06 +0200

    Update and rename slave_pico to slave_pico.py

--- ADVANCE_test/slave_pico.py
+++ ADVANCE_test/slave_pico.py
@@ -1,3 +1,20 @@
+# slave_pico.py
+# Copyright 2004 - 2024  biCOMM Design Ltd
+#
+# AUTH: Kostadin  Tosev
+# DATE: 2024
+#
+# Target: RPi5
+# Project CIS3
+# Hardware PCB V3.0
+# Tool: Python 3
+#
+# Version: V01.01.09.2024.CIS3 - temporary 01
+# 1. TestSPI,ADC - work. Measurement Voltage 0-10 V, resistive 0-1000 ohm
+# 2. Test Power PI5V/4.5A - work
+# 3. Test ADC communication - work
+# 4. Test LIN communication - work
+
 from machine import Pin, UART
 import time
 import struct
