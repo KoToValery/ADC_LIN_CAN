4194973cc5556e1fe11874c097ce5637d5e4c9d3
KoToValery <kostadintosev@gmail.com>
2025-01-24 22:19:11 +0200

    Update logger_config.py

--- ADC_LIN_CAN_25/logger_config.py
+++ ADC_LIN_CAN_25/logger_config.py
@@ -1,15 +1,15 @@
 # logger_config.py
-# Конфигурация на логването в файл и на конзола.
+# Конфигурация на логовете
 
 import logging
 
 logging.basicConfig(
-    level=logging.INFO,  # Set to DEBUG for more verbose output
+    level=logging.INFO,
     format='[%(asctime)s] [%(name)s] %(levelname)s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
     handlers=[
-        logging.FileHandler("adc_app.log"),  # Log to file
-        logging.StreamHandler()              # Log to console
+        logging.FileHandler("adc_app.log"),
+        logging.StreamHandler()
     ]
 )
 
