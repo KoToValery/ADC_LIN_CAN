04bf027ca090923f2e78c6824ca920da71c3408d
KoToValery <kostadintosev@gmail.com>
2025-01-25 11:53:48 +0200

    Update and rename data_structures.py to shared_data.py

---
+++ ADC_LIN_CAN_25/shared_data.py
@@ -0,0 +1,19 @@
+# shared_data.py
+
+# Latest sensor data to be served via HTTP, WebSockets, etc.
+latest_data = {
+    "adc_channels": {
+        "channel_0": {"voltage": 0.0, "unit": "V"},
+        "channel_1": {"voltage": 0.0, "unit": "V"},
+        "channel_2": {"voltage": 0.0, "unit": "V"},
+        "channel_3": {"voltage": 0.0, "unit": "V"},
+        "channel_4": {"resistance": 0.0, "unit": "Ω"},
+        "channel_5": {"resistance": 0.0, "unit": "Ω"}
+    },
+    "slave_sensors": {
+        "slave_1": {
+            "Temperature": 0.0,
+            "Humidity": 0.0
+        }
+    }
+}
