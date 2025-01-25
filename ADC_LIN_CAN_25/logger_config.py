bc9727d085bd87d3412792f21cc9096733b3a9e2
KoToValery <kostadintosev@gmail.com>
2025-01-25 11:52:32 +0200

    Update logger_config.py

--- ADC_LIN_CAN_25/logger_config.py
+++ ADC_LIN_CAN_25/logger_config.py
@@ -1,17 +1,12 @@
 # logger_config.py
-# Конфигурация на логването в файл и на конзола.
 
 import logging
 
 logging.basicConfig(
-    level=logging.INFO,  # Set to DEBUG for more verbose output
+    level=logging.INFO,  # Set to DEBUG for more verbose output if needed
     format='[%(asctime)s] [%(name)s] %(levelname)s: %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
-    handlers=[
-        logging.FileHandler("adc_app.log"),  # Log to file
-        logging.StreamHandler()              # Log to console
-    ]
+    datefmt='%Y-%m-%d %H:%M:%S'
 )
 
-logger = logging.getLogger('ADC, LIN & MQTT')
-logger.info("ADC, LIN & MQTT Add-on started.")
+logger = logging.getLogger("ADC, LIN & MQTT")
+logger.info("Logger initialized.")
